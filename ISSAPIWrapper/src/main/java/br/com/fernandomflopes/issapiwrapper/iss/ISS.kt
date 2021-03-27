3fdcdcb0639a83ceffe5572a6ecb1673a824ca44
Fernando Lopes <contato.fernandomflopes@gmail.com>
2021-03-27 11:26:20 -0300

    Add flow support

--- ISSAPIWrapper/src/main/java/br/com/fernandomflopes/issapiwrapper/iss/ISS.kt
+++ ISSAPIWrapper/src/main/java/br/com/fernandomflopes/issapiwrapper/iss/ISS.kt
@@ -2,6 +2,11 @@ package br.com.fernandomflopes.issapiwrapper.iss
 
 import br.com.fernandomflopes.issapiwrapper.model.ISSNow
 import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import okhttp3.OkHttp
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -21,4 +26,12 @@ class ISS {
         return Gson().fromJson(responseText, ISSNow::class.java)
     }
 
+    fun getNow(delayInMillis: Long) : Flow<ISSNow> {
+        return flow<ISSNow> {
+            while(true) {
+                emit(getNow())
+                delay(delayInMillis)
+            }
+        }.flowOn(Dispatchers.IO)
+    }
 }
--- app/src/main/java/br/com/fernandomflopes/iss/MainActivity.kt
+++ app/src/main/java/br/com/fernandomflopes/iss/MainActivity.kt
@@ -2,10 +2,35 @@ package br.com.fernandomflopes.iss
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import br.com.fernandomflopes.iss.databinding.ActivityMainBinding
+import br.com.fernandomflopes.issapiwrapper.iss.ISS
+import br.com.fernandomflopes.issapiwrapper.model.ISSNow
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        lifecycleScope.launch {
+            ISS().getNow(1000).collect {
+                binding.txtLongitude.text = it.position.longitude.toString()
+                binding.txtLatitude.text = it.position.latitude.toString()
+            }
+        }
     }
 }
